e31e6237d7110922eb5fb8ed3a22e7c5ed8654ab
Brenda Salinas <brenda.salinas2128@gmail.com>
2021-12-09 19:02:18 -0600

    developing local storage

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -16,13 +16,41 @@ hours.forEach(hour => {
     $('#container').append(`
         <div class = "row mt-2">
             <div class = "col-md-1 hour">${hour}</div>
-            <textarea class="col-md-10 future time-block"/></textarea>
-            <span class="col-md-1 saveBtn ">
+            <textarea id = "text1" class="col-md-10 future time-block"/></textarea>
+            <button class="col-md-1 saveBtn ">
                 <i class=" far fa-save"></i>
-            </span>
+            </button>
         </div>
     `)
 });
+
+// local storage
+var time1 = document.getElementById('text1');
+var button1 = document.getElementsByClassName('saveBtn');
+
+var storedInput = localStorage.getItem('task')
+
+
+if (time1){
+    time1.textContent = storedInput;
+}
+
+time1.addEventListener('input',letter => {
+    time1.textContent = letter.target.value;
+});
+
+
+var saveToLocalStorage = () => {
+    localStorage.setItem('task', time1.textContent)
+
+};
+
+for (i=0; i<button1.length; i++) {
+    button1[i].addEventListener('click', function(){
+        var value = $(this).siblings(".time-block").val()
+        localStorage.setItem('task', value)
+    });
+};
 
 
 
